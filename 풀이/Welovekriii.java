fe593a9017735cc9d0e87cad409f9f88fcf0dd80
wrjssmjdhappy <50691225+DongGeon0908@users.noreply.github.com>
2020-06-29 15:35:22 +0900

    Create Welovekriii.java

---
+++ 풀이/Welovekriii.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 2; i++) {
+			System.out.println("강한친구 대한육군");
+		}
+	}
+}

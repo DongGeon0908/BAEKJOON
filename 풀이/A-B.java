a4815d3a946eaa1ee150eb7630f61495ce244367
wrjssmjdhappy <50691225+DongGeon0908@users.noreply.github.com>
2020-06-29 15:32:40 +0900

    Create A-B.java

---
+++ 풀이/A-B.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class num_1001 {
+
+	public static int minus() {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		return a - b;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(minus());
+	}
+
+}

0ef25980ad84984935db038f0079f1221280ce50
wrjssmjdhappy <50691225+DongGeon0908@users.noreply.github.com>
2020-06-29 15:38:25 +0900

    Create AXB.java

---
+++ 풀이/AXB.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+
+		int num1 = scan.nextInt();
+		int num2 = scan.nextInt();
+
+		System.out.println(num1 * num2);
+	}
+}

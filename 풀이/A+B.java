c6d9215ac2e7add24cc29656c1a6856440388b47
wrjssmjdhappy <50691225+DongGeon0908@users.noreply.github.com>
2020-06-29 15:12:09 +0900

    A+B.java
    Scanner를 통해서 입력받은 값을 덧셈연산을 통해서 출력!

---
+++ 풀이/A+B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class num_1000 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		System.out.println(a+b);
+	}
+
+}

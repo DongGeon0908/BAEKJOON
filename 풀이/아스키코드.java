a05600e5114b13876a67a66f52991e52676471be
wrjssmjdhappy <50691225+DongGeon0908@users.noreply.github.com>
2020-06-29 15:42:52 +0900

    Create 아스키코드.java

---
+++ 풀이/아스키코드.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		String num = scan.nextLine();
+		int asci = num.charAt(0);
+		System.out.println(asci);
+	}
+}

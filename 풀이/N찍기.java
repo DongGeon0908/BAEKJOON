7c87b7371334d3abcb1b2993aa77b1444cc57a97
wrjssmjdhappy <50691225+DongGeon0908@users.noreply.github.com>
2020-06-29 15:39:41 +0900

    Create N찍기.java

---
+++ 풀이/N찍기.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+
+		int num = scan.nextInt();
+		int i = 1;
+
+		while (i <= num) {
+			System.out.println(i);
+			i++;
+		}
+	}
+}

2858be81171e8e47535973e696aafe206f979152
wrjssmjdhappy <50691225+DongGeon0908@users.noreply.github.com>
2020-06-30 14:31:37 +0900

    Create A나누기B.java

---
+++ 풀이/A나누기B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		double num1 = scan.nextInt();
+		double num2 = scan.nextInt();
+
+		System.out.println(num1 / num2);
+	}
+}

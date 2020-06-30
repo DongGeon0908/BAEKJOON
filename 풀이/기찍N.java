8ba2c1182e6493b14c6fa07f02ef722c0da609af
wrjssmjdhappy <50691225+DongGeon0908@users.noreply.github.com>
2020-06-30 14:45:45 +0900

    Create 기찍N.java

---
+++ 풀이/기찍N.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+
+		for (int i = num; i > 0; i--) {
+			System.out.println(i);
+		}
+
+	}
+}

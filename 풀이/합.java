b0b832752a2252f35cacabebca5bb46532319f95
wrjssmjdhappy <50691225+DongGeon0908@users.noreply.github.com>
2020-07-03 19:23:12 +0900

    Create 합.java

---
+++ 풀이/합.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+
+		int result = 0;
+
+		for (int i = 1; i <= num; i++) {
+			result = result + i;
+		}
+
+		System.out.println(result);
+
+	}
+}

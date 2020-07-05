877e465a432f5b8932c958caf622d4b08f994f18
wrjssmjdhappy <50691225+DongGeon0908@users.noreply.github.com>
2020-07-05 13:06:53 +0900

    Create A+B-8.java

---
+++ 풀이/A+B-8.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader bufferR = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bufferW = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer Token;
+		int num = Integer.parseInt(bufferR.readLine());
+
+		for (int i = 0; i < num; i++) {
+			Token = new StringTokenizer(bufferR.readLine());
+			int num1 = Integer.parseInt(Token.nextToken());
+			int num2 = Integer.parseInt(Token.nextToken());
+			int result = num1 + num2;
+
+			bufferW.write("Case #" + (i + 1) + ": " + num1 + " + " + num2 + " = " + result + "\n");
+		}
+		bufferW.flush();
+	}
+}

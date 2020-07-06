cfdfea2e57cf10fec729bbb470dc5a2ed27eb642
wrjssmjdhappy <50691225+DongGeon0908@users.noreply.github.com>
2020-07-06 15:42:52 +0900

    Create 등록.java

---
+++ 풀이/등록.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bufferW = new BufferedWriter(new OutputStreamWriter(System.out));
+		bufferW.write("35" +"\n");
+		bufferW.write("wrjs");
+		bufferW.flush();
+	}
+}

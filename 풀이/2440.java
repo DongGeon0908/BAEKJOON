9bcbbb4b9acd73289c6b51133bf12313ebfe2bbe
wrjssmjdhappy <50691225+DongGeon0908@users.noreply.github.com>
2020-07-07 13:26:54 +0900

    Create 2440.java

---
+++ 풀이/2440.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+
+		for (int i = num; i > 0; i--) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}

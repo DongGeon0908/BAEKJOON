03c36d24fed72929adba9d2294e7eae2687265ff
wrjssmjdhappy <50691225+DongGeon0908@users.noreply.github.com>
2020-07-14 17:40:07 +0900

    타노스

---
+++ 풀이/15802.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		String data = scan.nextLine();
+
+		System.out.println(1);
+	}
+}

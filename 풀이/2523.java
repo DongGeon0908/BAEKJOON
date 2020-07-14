01b3c161e38b2ae770b6fd7350c5597381eb77b9
wrjssmjdhappy <50691225+DongGeon0908@users.noreply.github.com>
2020-07-14 16:34:00 +0900

    별 찍기 - 13

---
+++ 풀이/2523.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		int data = scan.nextInt();
+
+		for (int i = 0; i < data; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		for (int i = data; i > 0; i--) {
+			for (int j = 0; j < i-1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}

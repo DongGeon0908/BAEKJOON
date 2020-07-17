5f974386c6859feb59787b7d1ec970301d4a48c3
wrjssmjdhappy <50691225+DongGeon0908@users.noreply.github.com>
2020-07-17 19:38:38 +0900

    홍익대학교

---
+++ 풀이/16394.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int count = scan.nextInt();
+		System.out.print(count-1946);
+	}
+}

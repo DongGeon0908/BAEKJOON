518749baa8f112209a97ff405a98ca57ee2fbf29
wrjssmjdhappy <50691225+DongGeon0908@users.noreply.github.com>
2020-07-23 14:14:23 +0900

    정수의 개수

---
+++ 풀이/10821.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String data = scan.nextLine();
+
+		String[] a = data.split(",");
+		System.out.print(a.length);
+
+	}
+}

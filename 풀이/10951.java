fa0e4952c220ebc4fdec788abda2883d0c5c9b9c
wrjssmjdhappy <50691225+DongGeon0908@users.noreply.github.com>
2020-07-24 12:20:11 +0900

    A+B - 4

---
+++ 풀이/10951.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		while (scan.hasNextInt()) {
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+
+			System.out.println(a + b);
+		}
+	}
+}

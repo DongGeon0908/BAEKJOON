67d1f2adc05877cdd3bc78f77a75bb6fd667ca12
wrjssmjdhappy <50691225+DongGeon0908@users.noreply.github.com>
2020-07-25 20:44:30 +0900

    별 찍기 - 9

---
+++ 풀이/2446.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int count = scan.nextInt();
+
+		for (int i = count; i >= 1; i--) {
+			for (int j = 0; j < count - i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= 2 * i - 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		for (int i = 2; i <= count; i++) {
+			for (int j = 0; j < count - i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= 2 * i - 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}

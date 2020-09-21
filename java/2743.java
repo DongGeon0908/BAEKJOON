82fdb8c35e9b4451f287c86570cb7dbf2aadf99c
DongGeon0908 <50691225+DongGeon0908@users.noreply.github.com>
2020-09-21 11:43:41 +0900

    문자열 구하기는 쉽지

---
+++ java/2743.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String data = scan.nextLine();
+
+		System.out.println(data.length());
+
+	}
+}

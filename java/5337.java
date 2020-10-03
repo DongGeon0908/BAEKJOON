e309780bf8622c4b42ddbd4583ab3dd81a580a7c
DongGeon0908 <50691225+DongGeon0908@users.noreply.github.com>
2020-10-03 10:28:23 +0900

    Update 5337.java

--- java/5337.java
+++ java/5337.java
@@ -1,8 +1,6 @@
-import java.util.Scanner;
-
 public class Main {
 	public static void main(String[] args) {
 
-		System.out.println(".  .   .\r\n" + "|  | _ | _. _ ._ _  _\r\n" + "|/\\|(/.|(_.(_)[ | )(/.");
+		System.out.println(".  .   .\n" + "|  | _ | _. _ ._ _  _\n" + "|/\\|(/.|(_.(_)[ | )(/.\n");
 	}
 }

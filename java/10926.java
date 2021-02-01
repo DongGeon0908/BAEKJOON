11c2640d513deef0985a5db6b230724a24071c8e
DongGeon0908 <50691225+DongGeon0908@users.noreply.github.com>
2021-02-01 12:11:10 +0900

    하루하루

---
+++ java/10926.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		String tmp = scan.nextLine();
+
+		tmp = tmp + "??!";
+		System.out.println(tmp);
+	}
+}

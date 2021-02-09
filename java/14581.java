efedc8bdc8404ee3b5b6cd58cc333c67cdaf1eee
DongGeon0908 <50691225+DongGeon0908@users.noreply.github.com>
2021-02-09 11:47:14 +0900

    zzzzzz

---
+++ java/14581.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String data = scan.nextLine();
+
+		System.out.println(":fan::fan::fan:");
+		System.out.println(":fan::" + data + "::fan:");
+		System.out.println(":fan::fan::fan:");
+	}
+}

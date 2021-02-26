ec124c81ff90efc956e4aa8276a42170ef8dc318
DongGeon0908 <50691225+DongGeon0908@users.noreply.github.com>
2021-02-26 11:40:06 +0900

    ...

---
+++ java/13597.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		int A = scan.nextInt();
+		int B = scan.nextInt();
+
+		if (A > B) {
+			System.out.println(A);
+		} else {
+			System.out.println(B);
+		}
+	}
+}
